e53b7bda3687db953585d71d7763420d9e324a25
Reyanshflas <reyanshflasplayz@gmail.com>
2024-12-07 10:13:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function Rock(Hello){
+    document.getElementById("para").innerText = "hello"
+}
+function John_Cena(){
+    document.getElementById("para").innerText = ""
+}
+
+function subtract(x, y){
+document.getElementById("para-2").innerText = x-y
+}
